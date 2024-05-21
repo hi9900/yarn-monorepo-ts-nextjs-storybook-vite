7e4df7c64011c3ba6f73da90d3f4b573bc783e58
heeyoung <9hiyoung@gmail.com>
2024-05-21 19:15:11 +0900

    Add: 레포지토리 세팅

---
+++ packages/web/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from '@/components/Navbar'
+
+export default function Page() {
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <h1>컴포넌트dd</h1>
+      </main>
+    </>
+  )
+}
